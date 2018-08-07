07e60d76743acaa6d545a218d35a416a5cddfbc8
Ken Brummage <KBrummage@gmail.com>
2018-08-06 21:45:21 -0700

    90%

--- assets/app.js
+++ assets/app.js
@@ -18,51 +18,47 @@ var database;
 $(document).ready(function () {
 
   displayTime();
-
-var config = {
-  apiKey: "AIzaSyD7rntgC4QnbllAUURTh1OVnwRUq3gv0W4",
-  authDomain: "pugetsoundtransit.firebaseapp.com",
-  databaseURL: "https://pugetsoundtransit.firebaseio.com",
-  projectId: "pugetsoundtransit",
-  storageBucket: "pugetsoundtransit.appspot.com",
-  messagingSenderId: "877703187461"
-};
-
-//initialize
-firebase.initializeApp(config);
-
-
-//firebase is now connected to my .js  
-// 'ref' is how we get info.  Each entry has an id.
-database = firebase.database();
-var TrainData = database.ref('TrainData');
-
-TrainData.on('value', gotData, errData);
-
-function gotData(data){
-console.log(data.val());
-
+for(var i=0; i < allStations.length; i++){
+  if(allStations[i] !== "Seattle"){
+    $("#DestinationSelect").append(`<option>${allStations[i]}</option>`)
+  }
 }
-function errData(err){
-  console.log('Error!');
-  console.log(err);
-}
+  
+  var config = {
+    apiKey: "AIzaSyD7rntgC4QnbllAUURTh1OVnwRUq3gv0W4",
+    authDomain: "pugetsoundtransit.firebaseapp.com",
+    databaseURL: "https://pugetsoundtransit.firebaseio.com",
+    projectId: "pugetsoundtransit",
+    storageBucket: "pugetsoundtransit.appspot.com",
+    messagingSenderId: "877703187461"
+  };
 
-  $("#info").append(`<div class="headline">${currentTime}</div>`);
+  //initialize
+  firebase.initializeApp(config);
+
+
+  //firebase is now connected to my .js  
+  // 'ref' is how we get info.  Each entry has an id.
+  database = firebase.database();
+  var TrainData = database.ref('TrainData');
+
+  TrainData.on('value', gotData, errData);
+
+  function gotData(data) {
+    console.log(data.val());
+
+  }
+
+  function errData(err) {
+    console.log('Error!');
+    console.log(err);
+  }
 
   $("button").click(function () {
-   
-    
+    console.log($(this).text());
 
-    });
 
-    
-    // console.log(dataRef);
-    // $("#info").append(`<div><div class="headline">`)
+  });
 
-    // dataRef.ref().push({
 
-    //   Station: station,
-    //   // Destination: destination,
-    // });
-  })
+})
